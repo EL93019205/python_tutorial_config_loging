26f1a5586a9e43764346ee1a71baaf712378ac99
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-11 04:46:43 +0900

    email送信を行おう

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 """
-CRITICAL
-ERROR
-WARNING
-INFO
-DEBUG
+main
 """
-import logging.config
+from email import message
+import smtplib
+import account
 
-LOGGER = logging.getLogger(__name__)
 
-LOGGER.error('api call is failed')
+SMTP_HOST = 'smtp.gmail.com'
+SMTP_PORT = 587
+FROM_EMAIL = account.from_email()
+TO_EMAIL = account.to_email()
+USERNAME = account.username()
+PASSWORD = account.password()
 
-LOGGER.error({
-    'action': 'create',
-    'status': 'fail',
-    'message': 'Api call is failed'
-})
+MSG = message.EmailMessage()
+MSG.set_content('Test email')
+MSG['Subject'] = 'Test email sub'
+MSG['From'] = FROM_EMAIL
+MSG['To'] = TO_EMAIL
+
+SERVER = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+SERVER.ehlo()
+SERVER.starttls()
+SERVER.ehlo()
+SERVER.login(USERNAME, PASSWORD)
+SERVER.send_message(MSG)
+SERVER.quit()
