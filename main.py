c0c573481f58a870a119725671667de49ac1e023
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-11 14:20:47 +0900

    SMTPハンドラーでログをemail送信しよう

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 """
 main
 """
-from email.mime import multipart
-from email.mime import text
-import smtplib
+import logging
+import logging.handlers
 
 
 import account
@@ -16,24 +15,15 @@ TO_EMAIL = account.to_email()
 USERNAME = account.username()
 PASSWORD = account.password()
 
-MSG = multipart.MIMEMultipart()
-MSG['Subject'] = 'Test email sub'
-MSG['From'] = FROM_EMAIL
-MSG['To'] = TO_EMAIL
-MSG.attach(text.MIMEText('Test email', 'plain'))
+LOGGER = logging.getLogger('email')
+LOGGER.setLevel(logging.CRITICAL)
 
-with open('main.py', 'r') as f:
-    attachment = text.MIMEText(f.read(), 'plain')
-    attachment.add_header(
-        'Content-Disposition', 'attachment',
-        filename='lesson.txt'
-    )
-    MSG.attach(attachment)
+LOGGER.addHandler(logging.handlers.SMTPHandler(
+    (SMTP_HOST, SMTP_PORT), FROM_EMAIL, TO_EMAIL,
+    subject='Admin test log', credentials=(USERNAME, PASSWORD),
+    secure=(None, None, None),
+    timeout=20
+))
 
-SERVER = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
-SERVER.ehlo()
-SERVER.starttls()
-SERVER.ehlo()
-SERVER.login(USERNAME, PASSWORD)
-SERVER.send_message(MSG)
-SERVER.quit()
+LOGGER.info('test')
+LOGGER.critical('critical')
