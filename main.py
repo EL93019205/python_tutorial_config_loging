9cc9c477bf345753aa0cab94714a98a3d54de395
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-11 15:30:46 +0900

    optparseを扱おう

--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 """
 main
 """
-import logging
-import logging.handlers
+# pylint: disable=W0402
+from optparse import OptionParser
+from optparse import OptionGroup
 
 
-import account
+def main():
+    """
+    main
+    """
+    usage = 'usage: %prog [options] arg1 arg2'
+    parser = OptionParser(usage=usage)
+    parser.add_option('-f', '--file', action='store', type='string',
+                      dest='filename', help='File name')
+    parser.add_option('-n', '--num', action='store', type='int', dest='num')
+    parser.set_defaults(verbose=True)
+    parser.add_option('-v', action='store_false', dest='verbose')
+    parser.add_option('-q', action='store_true', dest='verbose')
+    parser.add_option('-r', action='store_const', const='root',
+                      dest='user_name')
+    parser.add_option('-e', dest='env')
+
+    def is_release(option, opt_str, value, parser):
+        if parser.values.env == 'prd':
+            raise parser.error("Can't release")
+        setattr(parser.values, option.dest, True)
+        print(opt_str, value)
+    parser.add_option('--release', action='callback', callback=is_release,
+                      dest='release')
+    group = OptionGroup(parser, 'Dangerous options')
+    group.add_option('-g', action='store_true', help='Group option')
+    parser.add_option_group(group)
+
+    options, args = parser.parse_args()
+    print(options)
+    print(args)
+    print(type(options))
+    print(type(args))
 
 
-SMTP_HOST = 'smtp.gmail.com'
-SMTP_PORT = 587
-FROM_EMAIL = account.from_email()
-TO_EMAIL = account.to_email()
-USERNAME = account.username()
-PASSWORD = account.password()
-
-LOGGER = logging.getLogger('email')
-LOGGER.setLevel(logging.CRITICAL)
-
-LOGGER.addHandler(logging.handlers.SMTPHandler(
-    (SMTP_HOST, SMTP_PORT), FROM_EMAIL, TO_EMAIL,
-    subject='Admin test log', credentials=(USERNAME, PASSWORD),
-    secure=(None, None, None),
-    timeout=20
-))
-
-LOGGER.info('test')
-LOGGER.critical('critical')
+if __name__ == '__main__':
+    main()
